6211aa1f29d4336a779b3ce700bd0b1a2372f79d
Murad Malik <muradmalik7@gmail.com>
2021-06-08 04:19:47 +0500

    removed package

--- src/components/CryptoItem.tsx
+++ src/components/CryptoItem.tsx
@@ -5,7 +5,7 @@ import { useMutation } from '@apollo/client';
 import { DELETE_CRYPTO_FROM_WALLET } from '../operations/mutations/crypto';
 import { RefetchWallet } from './PaymentsAccount';
 import { formatFloat } from '../helpers/formatter';
-import icons from 'base64-cryptocurrency-icons';
+// import icons from 'base64-cryptocurrency-icons';
 
 interface Props {
   id: string;
@@ -30,7 +30,7 @@ export const CryptoItem: React.FC<Props> = ({ name, balance, id, refetchWallet }
     <Grid container item direction={'row'} className="list-row">
       <Grid item xs={4} className="list-item">
         <Box className="coinTitle" display="flex" flexDirection="row" justifyContent="flex-start" alignItems="center">
-          <img src={generateIcon(name)} alt="raiinmaker" className="coinIcon" />
+          {/* <img src={generateIcon(name)} alt="raiinmaker" className="coinIcon" /> */}
           <Typography>{name.toUpperCase()}</Typography>
         </Box>
       </Grid>
