de9cd913990ff0c47080380e4715a0dd32c8317f
Murad Malik <42974204+muradmalik777@users.noreply.github.com>
2021-08-05 14:49:25 +0500

    topbar refactored and redesigned

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,102 +1,30 @@
 import React from 'react';
 import clsx from 'clsx';
-import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import { Switch } from 'react-router';
 import { CampaignsList } from '../../components/CampaignsList';
 import { NewCampaign } from '../../components/campaign-create/NewCampaign';
 import { Link, useHistory } from 'react-router-dom';
 import { MarketData } from '../../components/MarketData';
 import { DashboardHome } from '../../components/DashboardHome';
-
 import SettingsIcon from '@material-ui/icons/Settings';
 import { PaymentsAccount } from '../../components/PaymentsAccount';
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 import { ProtectedRoute, UserContext } from '../../components/ProtectedRoute';
-import { Button, Grid, Box } from '@material-ui/core';
+import { Box } from '@material-ui/core';
 import { graphqlClient, sessionLogout } from '../../clients/raiinmaker-api';
-
 import { Admin } from '../../components/Admin';
 import { ManageWithdrawRequests } from '../../components/admin/ManageWithdrawRequests';
 import { CampaignAudit } from '../../components/admin/CampaignAudit';
 import { UserManagement } from '../../components/UserManagement';
 import { CampaignAuditList } from '../../components/admin/CampaignAuditList';
 import Sidebar from '../../components/Sidebar';
-
-const drawerWidth = 240;
-
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      display: 'flex',
-    },
-    appBar: {
-      transition: theme.transitions.create(['margin', 'width'], {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.leavingScreen,
-      }),
-    },
-    appBarShift: {
-      width: `calc(100% - ${drawerWidth}px)`,
-      marginLeft: drawerWidth,
-      transition: theme.transitions.create(['margin', 'width'], {
-        easing: theme.transitions.easing.easeOut,
-        duration: theme.transitions.duration.enteringScreen,
-      }),
-    },
-    menuButton: {
-      marginRight: theme.spacing(2),
-    },
-    hide: {
-      display: 'none',
-    },
-    drawer: {
-      width: drawerWidth,
-      flexShrink: 0,
-    },
-    drawerPaper: {
-      width: drawerWidth,
-    },
-    drawerHeader: {
-      display: 'flex',
-      alignItems: 'center',
-      padding: theme.spacing(0, 1),
-      // necessary for content to be below app bar
-      ...theme.mixins.toolbar,
-      justifyContent: 'flex-end',
-    },
-    content: {
-      flexGrow: 1,
-      padding: theme.spacing(3),
-      transition: theme.transitions.create('margin', {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.leavingScreen,
-      }),
-      marginLeft: -drawerWidth,
-    },
-    contentShift: {
-      transition: theme.transitions.create('margin', {
-        easing: theme.transitions.easing.easeOut,
-        duration: theme.transitions.duration.enteringScreen,
-      }),
-      marginLeft: 0,
-    },
-  }),
-);
+import styles from './Dashboard.module.scss';
 
 const Dashboard: React.FC = (props) => {
-  const classes = useStyles();
   const [open, setOpen] = React.useState(true);
 
   const history = useHistory();
-
-  const handleDrawerOpen = () => {
-    setOpen(true);
-  };
 
   const handleLogout = async () => {
     const res = await sessionLogout();
@@ -112,96 +40,65 @@ const Dashboard: React.FC = (props) => {
     <UserContext.Consumer>
       {(value) => {
         return (
-          <div className={classes.root}>
-            <CssBaseline />
-            <AppBar
-              position="fixed"
-              className={clsx(classes.appBar, {
-                [classes.appBarShift]: open,
-              })}
-            >
-              <Toolbar>
-                <IconButton
-                  color="inherit"
-                  aria-label="open drawer"
-                  onClick={handleDrawerOpen}
-                  edge="start"
-                  className={clsx(classes.menuButton, open && classes.hide)}
-                >
-                  <MenuIcon />
-                </IconButton>
-                <Grid container direction={'row'} justify={'flex-end'}>
-                  <Grid item>
-                    <Button style={{ backgroundColor: 'transparent' }}>
-                      <Link to={'/dashboard/paymentsAccount'} style={{ color: 'white' }}>
-                        <SettingsIcon />
-                      </Link>
-                    </Button>
-                  </Grid>
-                  <Grid item>
-                    <Button onClick={handleLogout} style={{ backgroundColor: 'transparent', color: 'white' }}>
-                      <ExitToAppIcon />
-                    </Button>
-                  </Grid>
-                </Grid>
-              </Toolbar>
-            </AppBar>
-
-            <Box className="w-72">
+          <Box className="box-border w-screen flex flex-row flex-nowrap">
+            <Box className={styles.side}>
               <Sidebar value={value} />
             </Box>
-
-            <main
-              className={`${clsx(classes.content, {
-                [classes.contentShift]: open,
-              })} relative full-screen-height
-              `}
-            >
-              <div className={classes.drawerHeader} />
-              <Switch>
-                <ProtectedRoute exact path={'/dashboard'}>
-                  <DashboardHome />
-                </ProtectedRoute>
-                <ProtectedRoute exact path={'/dashboard/campaigns'}>
-                  <CampaignsList />
-                </ProtectedRoute>
-                <ProtectedRoute exact path={'/dashboard/newCampaign'}>
-                  <NewCampaign userData={value} {...props} />
-                </ProtectedRoute>
-                <ProtectedRoute exact path={'/dashboard/marketData'}>
-                  <MarketData />
-                </ProtectedRoute>
-                <ProtectedRoute exact path={'/dashboard/paymentsAccount'}>
-                  <PaymentsAccount />
-                </ProtectedRoute>
-                {value['role'] === 'admin' && (
-                  <ProtectedRoute exact path={'/dashboard/admin/userManagement'}>
-                    <UserManagement {...props} />
+            <Box className={styles.content}>
+              <Box className="w-full flex flex-row justify-end items-center px-4 py-3 bg-gray-100 mb-10">
+                <Link className="mr-2" to={'/dashboard/paymentsAccount'}>
+                  <SettingsIcon />
+                </Link>
+                <IconButton onClick={handleLogout}>
+                  <ExitToAppIcon />
+                </IconButton>
+              </Box>
+              <Box className="w-full">
+                <Switch>
+                  <ProtectedRoute exact path={'/dashboard'}>
+                    <DashboardHome />
                   </ProtectedRoute>
-                )}
-                {value['company'] === 'raiinmaker' && (
-                  <ProtectedRoute exact path={'/dashboard/admin/management'} adminOnly={true}>
-                    <Admin {...props} />
+                  <ProtectedRoute exact path={'/dashboard/campaigns'}>
+                    <CampaignsList />
                   </ProtectedRoute>
-                )}
-                {value['company'] === 'raiinmaker' && (
-                  <ProtectedRoute exact path={'/dashboard/admin/withdraw'} adminOnly={true}>
-                    <ManageWithdrawRequests {...props} />
+                  <ProtectedRoute exact path={'/dashboard/newCampaign'}>
+                    <NewCampaign userData={value} {...props} />
                   </ProtectedRoute>
-                )}
-                {value['role'] === 'admin' && (
-                  <ProtectedRoute exact path={'/dashboard/admin/campaign-audit'}>
-                    <CampaignAudit {...props} />
+                  <ProtectedRoute exact path={'/dashboard/marketData'}>
+                    <MarketData />
                   </ProtectedRoute>
-                )}
-                {value['role'] === 'admin' && (
-                  <ProtectedRoute exact path={'/dashboard/admin/audit-campaigns'}>
-                    <CampaignAuditList {...props} />
+                  <ProtectedRoute exact path={'/dashboard/paymentsAccount'}>
+                    <PaymentsAccount />
                   </ProtectedRoute>
-                )}
-              </Switch>
-            </main>
-          </div>
+                  {value['role'] === 'admin' && (
+                    <ProtectedRoute exact path={'/dashboard/admin/userManagement'}>
+                      <UserManagement {...props} />
+                    </ProtectedRoute>
+                  )}
+                  {value['company'] === 'raiinmaker' && (
+                    <ProtectedRoute exact path={'/dashboard/admin/management'} adminOnly={true}>
+                      <Admin {...props} />
+                    </ProtectedRoute>
+                  )}
+                  {value['company'] === 'raiinmaker' && (
+                    <ProtectedRoute exact path={'/dashboard/admin/withdraw'} adminOnly={true}>
+                      <ManageWithdrawRequests {...props} />
+                    </ProtectedRoute>
+                  )}
+                  {value['role'] === 'admin' && (
+                    <ProtectedRoute exact path={'/dashboard/admin/campaign-audit'}>
+                      <CampaignAudit {...props} />
+                    </ProtectedRoute>
+                  )}
+                  {value['role'] === 'admin' && (
+                    <ProtectedRoute exact path={'/dashboard/admin/audit-campaigns'}>
+                      <CampaignAuditList {...props} />
+                    </ProtectedRoute>
+                  )}
+                </Switch>
+              </Box>
+            </Box>
+          </Box>
         );
       }}
     </UserContext.Consumer>
