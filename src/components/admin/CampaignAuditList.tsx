f26602d094d84a14dcc10e403d64503968276431
Arslan Shahab <arslanshahab@gmail.com>
2021-12-06 19:10:43 +0500

    Added modal for campaigns-audit

--- src/components/admin/CampaignAuditList.tsx
+++ src/components/admin/CampaignAuditList.tsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
-import { useLazyQuery } from '@apollo/client';
+import { useQuery } from '@apollo/client';
 import { ADMIN_LIST_CAMPAIGN_QUERY } from '../../operations/queries/admin';
-import { useHistory } from 'react-router';
 import { Box, Button, CircularProgress } from '@material-ui/core';
 import { Campaign } from '../../types';
+import GenericModal from './../GenericModal';
 
 interface Props {
   location?: {
@@ -15,10 +15,11 @@ interface Props {
 }
 
 export const CampaignAuditList: React.FC<Props> = () => {
-  const history = useHistory();
   const [loaded, setLoaded] = useState(false);
   const [skip, setSkip] = useState(0);
-  const [getCampaigns, { loading, data }] = useLazyQuery(ADMIN_LIST_CAMPAIGN_QUERY, {
+  const [progressModal, showProgressModal] = useState(false);
+
+  const { loading, data } = useQuery(ADMIN_LIST_CAMPAIGN_QUERY, {
     variables: {
       open: false,
       scoped: true,
@@ -29,11 +30,12 @@ export const CampaignAuditList: React.FC<Props> = () => {
     },
     fetchPolicy: 'cache-and-network',
   });
-  // console.log('Campaign Audit List', skip);
+  console.log('Campaign Audit List', skip);
 
   const loadData = async (skip: number) => {
     try {
-      await getCampaigns();
+      debugger;
+      // await getCampaigns();
       await setSkip(skip);
     } catch (e) {
       console.log('Error: Load Data Error');
@@ -43,79 +45,82 @@ export const CampaignAuditList: React.FC<Props> = () => {
   };
 
   const handleClick = (data: any) => {
-    history.push('/dashboard/admin/campaign-audit', { data: data });
+    try {
+      showProgressModal(true);
+    } catch (e) {
+      showProgressModal(false);
+    }
+    // history.push('/dashboard/admin/campaign-audit', { data: data });
   };
 
-  const renderManageWithdrawals = () => {
-    if (!loaded) loadData(skip);
-    if (loading)
-      return (
-        <div className="flex justify-center items-center">
-          <CircularProgress></CircularProgress>
-        </div>
-      );
-    if (data) {
-      if (data.listCampaigns.results.length == 0)
-        return (
-          <div>
-            <p>No Auditable Campaigns Found</p>
-          </div>
-        );
-      return (
-        <div className="p-8">
-          <h1>Campaigns Pending to be Audited.</h1>
-          {skip != 0 ? (
-            <Button
-              variant="outlined"
-              onClick={async () => {
-                await loadData(skip - 10);
-              }}
-            >
-              <p>Previous</p>
-            </Button>
-          ) : (
-            <></>
-          )}
-          {skip + data.listCampaigns.results.length != data.listCampaigns.total ? (
-            <Button
-              variant="outlined"
-              onClick={async () => {
-                await loadData(skip + 10);
-              }}
-            >
-              <p>Next</p>
-            </Button>
-          ) : (
-            <></>
-          )}
-          <Box className="w-full pb-10 overflow-scroll">
-            <table className="w-full table-auto bg-gray-50">
-              <thead>
-                <tr className="font-semibold bg-gray-100">
-                  <th className="px-7 py-5 text-left">Campaign Name</th>
-                  <th className="px-7 py-5 text-left">Audited</th>
-                  <th className="px-7 py-5 text-left">Campaign Ended</th>
+  if (loading)
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <CircularProgress></CircularProgress>
+      </div>
+    );
+
+  return (
+    <div className="p-8">
+      <GenericModal
+        open={progressModal}
+        onClose={() => showProgressModal(false)}
+        persist={false}
+        size="mini"
+        showCloseIcon={true}
+      >
+        Hello
+      </GenericModal>
+      <h1 className="text-blue-900 font-semibold text-2xl pb-3">Campaigns Pending to be Audited.</h1>
+      {skip != 0 ? (
+        <Button
+          variant="outlined"
+          onClick={async () => {
+            await loadData(skip - 10);
+          }}
+        >
+          <p>Previous</p>
+        </Button>
+      ) : (
+        <></>
+      )}
+      {skip + data?.listCampaigns?.results?.length != data?.listCampaigns?.total ? (
+        <Button
+          variant="outlined"
+          onClick={async () => {
+            await loadData(skip + 10);
+          }}
+        >
+          <p>Next</p>
+        </Button>
+      ) : (
+        <></>
+      )}
+      <Box className="w-full pb-10 overflow-scroll">
+        <table className="w-full table-auto bg-gray-50">
+          <thead>
+            <tr className="font-semibold bg-gray-100">
+              <th className="px-7 py-5 text-left">Campaign Name</th>
+              <th className="px-7 py-5 text-left">Audited</th>
+              <th className="px-7 py-5 text-left">Campaign Ended</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data &&
+              data.listCampaigns.results.map((x: Campaign, index: number) => (
+                <tr
+                  className="hover:bg-gray-100 border-b-2 border-solid border-gray-100 cursor-pointer"
+                  key={x.id}
+                  onClick={() => handleClick(x)}
+                >
+                  <td className="px-7 py-5 text-left capitalize">{x.name}</td>
+                  <td className="px-7 py-5 text-left">{x.audited.toString()}</td>
+                  <td className="px-7 py-5 text-left">{new Date(parseInt(x.endDate)).toDateString()}</td>
                 </tr>
-              </thead>
-              <tbody>
-                {data &&
-                  data.listCampaigns.results.map((x: Campaign, index: number) => (
-                    <tr
-                      className="hover:bg-gray-100 border-b-2 border-solid border-gray-100 cursor-pointer"
-                      key={x.id}
-                      onClick={() => handleClick(x)}
-                    >
-                      <td className="px-7 py-5 text-left capitalize">{x.name}</td>
-                      <td className="px-7 py-5 text-left">{x.audited.toString()}</td>
-                      <td className="px-7 py-5 text-left">{new Date(parseInt(x.endDate)).toString()}</td>
-                    </tr>
-                  ))}
-              </tbody>
-            </table>
-          </Box>
-        </div>
-      );
-    }
-  };
-  return <div>{renderManageWithdrawals()}</div>;
+              ))}
+          </tbody>
+        </table>
+      </Box>
+    </div>
+  );
 };
