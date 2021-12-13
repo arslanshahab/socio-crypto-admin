f4954233df767ecb49a52c3d39b7673fe411df6c
Arslan Shahab <arslanshahab@gmail.com>
2021-12-13 17:16:01 +0500

    designed and refactored the pending-withdraw-list

--- src/components/Admin.tsx
+++ src/components/Admin.tsx
@@ -24,19 +24,14 @@ export const Admin: React.FC = (props) => {
   };
 
   return (
-    <div>
+    <div className="p-4">
       <RegisterBrand open={open} setOpen={setOpen} />
-      <Grid container>
-        <Grid item xs>
-          <Typography variant={'h4'}>Admin</Typography>
-        </Grid>
-        <Grid item xs={6} />
-        <Grid item xs>
-          <Button variant={'contained'} color={'primary'} onClick={() => setOpen(true)}>
-            Register New Brand
-          </Button>
-        </Grid>
-      </Grid>
+      <div className="flex justify-between items-center pb-4">
+        <h1 className="text-blue-900 font-semibold text-2xl">Admin</h1>
+        <Button variant={'contained'} color={'primary'} onClick={() => setOpen(true)}>
+          Register New Brand
+        </Button>
+      </div>
       <AppBar position="static">
         <Tabs value={value} onChange={handleChange} aria-label="simple tabs example">
           <Tab label="Payments / KYC" {...a11yProps(0)} />
--- src/components/admin/PendingWithdrawList/PendingWithdrawList.tsx
+++ src/components/admin/PendingWithdrawList/PendingWithdrawList.tsx
@@ -1,77 +1,60 @@
-import React, { useState } from 'react';
-import { useLazyQuery } from '@apollo/client';
+import React from 'react';
+import { useQuery } from '@apollo/client';
 import { GET_PENDING_WITHDRAWALS } from '../../../operations/queries/admin';
 import { useHistory } from 'react-router';
+import { CircularProgress } from '@material-ui/core';
 import styles from './pendingWithdrawList.module.css';
 
-export const PendingWithdrawList: React.FC = () => {
+export const PendingWithdrawList = () => {
   const history = useHistory();
-  const [loaded, setLoaded] = useState(false);
-  const [getPendingWithdrawals, { loading, data }] = useLazyQuery(GET_PENDING_WITHDRAWALS, {
-    fetchPolicy: 'network-only',
-  });
-
+  //! GraphQL Queries
+  const { data, loading } = useQuery(GET_PENDING_WITHDRAWALS, { fetchPolicy: 'network-only' });
+  //! Loading
+  if (loading) {
+    return (
+      <div className={styles.loading}>
+        <CircularProgress />
+      </div>
+    );
+  }
+  //! Functions
   const handleClick = (data: any) => {
-    history.push('/dashboard/admin/withdraw', { data: data });
+    history.push('/dashboard/admin/withdraw', { data });
   };
 
-  const loadData = async () => {
-    await getPendingWithdrawals();
-    await setLoaded(true);
-  };
-
-  const renderManageWithdrawals = () => {
-    if (!loaded) loadData();
-    if (loading) return <div></div>;
-    if (data) {
-      return (
-        <div className={styles.pendingWithdrawListWrapper}>
-          <h1>Pending Withdrawals</h1>
-          {/* //!-----------*********-------------  */}
-          <div className="w-full pb-10 overflow-scroll">
-            <table className="w-full table-auto bg-gray-50">
-              <thead>
-                <tr className="font-semibold bg-gray-100">
-                  <th className="px-7 py-5 text-left">Name</th>
-                  <th className="px-7 py-5 text-left">Transfered Withdrawals</th>
-                  <th className="px-7 py-5 text-left">Pending Withdrawals</th>
-                  <th className="px-7 py-5 text-left">Total Annual Withdrawn</th>
-                  <th className="px-7 py-5 text-left">KYC Status</th>
-                  <th className="px-7 py-5 text-left"></th>
-                </tr>
-              </thead>
-              <tbody>
-                {data?.getWithdrawalsV2.map((withdraw: any, index: number) => (
-                  <tr
-                    className="hover:bg-gray-100 border-b-2 border-solid border-gray-100"
-                    key={index}
-                    onClick={() => handleClick(withdraw)}
-                  >
-                    <td className="px-7 py-5 text-left capitalize">{withdraw.user.username}</td>
-                    <td className="px-7 py-5 text-left capitalize">{withdraw.transfers.length}</td>
-                    <td className="px-7 py-5 text-left capitalize">{withdraw.totalPendingWithdrawal.length}</td>
-                    <td className="px-7 py-5 text-left capitalize">{withdraw.totalAnnualWithdrawn}</td>
-                    <td className="px-7 py-5 text-left capitalize">{withdraw.user.kycStatus}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-          {/* //!-----------______----------------- */}
-          {/* {data.getWithdrawalsV2.map((withdraw: any) => {
-            return (
-              <div className="pending-withdraw" key={withdraw.user.id} onClick={() => handleClick(withdraw)}>
-                <ul>
-                  <li>{withdraw.user.username}</li>
-                  <li className="right">{`Total Pending Withdraw: ${withdraw.totalPendingWithdrawal.length} token types`}</li>
-                </ul>
-              </div>
-            );
-          })} */}
-        </div>
-      );
-    }
-  };
-
-  return <div>{renderManageWithdrawals()}</div>;
+  return (
+    <div className={styles.pendingWithdrawListWrapper}>
+      <h1 className={styles.withdrawalHeading}>Pending Withdrawals</h1>
+      <div className={styles.tableWrapper}>
+        <table className={styles.table}>
+          <thead>
+            <tr className={styles.tableHeadRow}>
+              <th className={styles.withdrawColumn}>Name</th>
+              <th className={styles.withdrawColumn}>Transfered Withdrawals</th>
+              <th className={styles.withdrawColumn}>Pending Withdrawals</th>
+              <th className={styles.withdrawColumn}>Total Annual Withdrawn</th>
+              <th className={styles.withdrawColumn}>KYC Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data?.getWithdrawalsV2.map((withdraw: any, index: number) => (
+              <tr className={styles.tableBodyRow} key={index} onClick={() => handleClick(withdraw)}>
+                <td className={styles.withdrawColumn}>{withdraw.user.username}</td>
+                <td className={styles.withdrawColumn}>{withdraw.transfers.length}</td>
+                <td className={styles.withdrawColumn}>{withdraw.totalPendingWithdrawal.length}</td>
+                <td className={styles.withdrawColumn}>${withdraw.totalAnnualWithdrawn}</td>
+                <td
+                  className={`${withdraw.user.kycStatus === 'pending' ? 'text-red-600' : 'text-green-600'} ${
+                    styles.withdrawColumn
+                  }`}
+                >
+                  {withdraw.user.kycStatus}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
