d8ba720b29b3204c033a1b760082badd4f9282c2
Arslan Shahab <arslanshahab@gmail.com>
2021-12-13 15:54:01 +0500

    added table record

--- src/components/admin/PendingWithdrawList/PendingWithdrawList.tsx
+++ src/components/admin/PendingWithdrawList/PendingWithdrawList.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useLazyQuery } from '@apollo/client';
 import { GET_PENDING_WITHDRAWALS } from '../../../operations/queries/admin';
 import { useHistory } from 'react-router';
+import styles from './pendingWithdrawList.module.css';
 
 export const PendingWithdrawList: React.FC = () => {
   const history = useHistory();
@@ -24,7 +25,7 @@ export const PendingWithdrawList: React.FC = () => {
     if (loading) return <div></div>;
     if (data) {
       return (
-        <div className="margin-bottom">
+        <div className={styles.pendingWithdrawListWrapper}>
           <h1>Pending Withdrawals</h1>
           {/* //!-----------*********-------------  */}
           <div className="w-full pb-10 overflow-scroll">
@@ -32,28 +33,32 @@ export const PendingWithdrawList: React.FC = () => {
               <thead>
                 <tr className="font-semibold bg-gray-100">
                   <th className="px-7 py-5 text-left">Name</th>
-                  <th className="px-7 py-5 text-left">KYC Status</th>
-                  <th className="px-7 py-5 text-left">Transfer Amount</th>
-                  <th className="px-7 py-5 text-left">Transfer Action</th>
-                  <th className="px-7 py-5 text-left">Pending Withdrawal Balance</th>
+                  <th className="px-7 py-5 text-left">Transfered Withdrawals</th>
+                  <th className="px-7 py-5 text-left">Pending Withdrawals</th>
                   <th className="px-7 py-5 text-left">Total Annual Withdrawn</th>
-                  <th className="px-7 py-5 text-left">Status</th>
+                  <th className="px-7 py-5 text-left">KYC Status</th>
                   <th className="px-7 py-5 text-left"></th>
                 </tr>
               </thead>
               <tbody>
                 {data?.getWithdrawalsV2.map((withdraw: any, index: number) => (
-                  <div key={index}>
-                    <tr className="hover:bg-gray-100 border-b-2 border-solid border-gray-100">
-                      <td className="px-7 py-5 text-left capitalize">{withdraw.user.username}</td>
-                    </tr>
-                  </div>
+                  <tr
+                    className="hover:bg-gray-100 border-b-2 border-solid border-gray-100"
+                    key={index}
+                    onClick={() => handleClick(withdraw)}
+                  >
+                    <td className="px-7 py-5 text-left capitalize">{withdraw.user.username}</td>
+                    <td className="px-7 py-5 text-left capitalize">{withdraw.transfers.length}</td>
+                    <td className="px-7 py-5 text-left capitalize">{withdraw.totalPendingWithdrawal.length}</td>
+                    <td className="px-7 py-5 text-left capitalize">{withdraw.totalAnnualWithdrawn}</td>
+                    <td className="px-7 py-5 text-left capitalize">{withdraw.user.kycStatus}</td>
+                  </tr>
                 ))}
               </tbody>
             </table>
           </div>
           {/* //!-----------______----------------- */}
-          {data.getWithdrawalsV2.map((withdraw: any) => {
+          {/* {data.getWithdrawalsV2.map((withdraw: any) => {
             return (
               <div className="pending-withdraw" key={withdraw.user.id} onClick={() => handleClick(withdraw)}>
                 <ul>
@@ -62,7 +67,7 @@ export const PendingWithdrawList: React.FC = () => {
                 </ul>
               </div>
             );
-          })}
+          })} */}
         </div>
       );
     }
