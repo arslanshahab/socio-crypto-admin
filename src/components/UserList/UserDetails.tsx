908538c83a556eaef429040cbf171195d1462db5
Arslan Shahab <arslanshahab@gmail.com>
2022-04-22 22:21:48 +0500

    implemented user wallet currency api

--- src/components/UserList/UserDetails.tsx
+++ src/components/UserList/UserDetails.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import headingStyles from '../../assets/styles/heading.module.css';
 import styles from './userList.module.css';
 
 const UserDetails: React.FC<any> = (props: any) => {
   const { userDetails } = props;
-  console.log('userDetails: ', userDetails);
+  const [curreny, setCurrency] = useState<any>();
+  useEffect(() => {
+    const fetchUserCurrencyDetails = async () => {
+      const response = await axios.get(`http://localhost:4000/v1/user/wallet-balances?userId=${userDetails.id}`);
+      setCurrency(response.data.data);
+    };
+    fetchUserCurrencyDetails();
+  }, [userDetails]);
+  console.log('userDetails: ', userDetails, '---------', curreny);
+
   return (
     <div className={styles.userDetailsWrapper}>
       <div className={styles.userSideWrapper}>
@@ -40,6 +50,32 @@ const UserDetails: React.FC<any> = (props: any) => {
       </div>
       <div className={styles.transferSide}>
         <h3 className={headingStyles.headingSm}>Transfer User Record</h3>
+        <h4 className={headingStyles.headingXs}>Coiin Amount:</h4>
+        {curreny &&
+          curreny?.map((x: any) => (
+            <div className="shadow-md rounded-sm my-2 p-2" key={userDetails.id}>
+              <div className={styles.boxStyle}>
+                <h4>Balance:</h4>
+                <p>{x.balance}</p>
+              </div>
+              <div className={styles.boxStyle}>
+                <h4>Minimum Withdraw Amount:</h4>
+                <p>{x.minWithdrawAmount}</p>
+              </div>
+              <div className={styles.boxStyle}>
+                <h4>USD Balance:</h4>
+                <p>{x.usdBalance}</p>
+              </div>
+              <div className={styles.boxStyle}>
+                <h4>Symbol:</h4>
+                <p>{x.symbol}</p>
+              </div>
+              <div className={styles.boxStyle}>
+                <h4>Network:</h4>
+                <p>{x.network}</p>
+              </div>
+            </div>
+          ))}
       </div>
     </div>
   );
