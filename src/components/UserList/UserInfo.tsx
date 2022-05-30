ebf8cc962896e322a187c2e8862458b10192569a
Arslan Shahab <arslanshahab@gmail.com>
2022-05-30 19:06:07 +0500

    integrated reset-user-password and get single-user api's

--- src/components/UserList/UserInfo.tsx
+++ src/components/UserList/UserInfo.tsx
@@ -1,98 +1,136 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import styles from './userList.module.css';
 import headingStyles from '../../assets/styles/heading.module.css';
 import buttonStyles from '../../assets/styles/customButton.module.css';
 import CustomButton from '../CustomButton';
-import { UserTypes } from '../../types';
+import { UserListType, UserTypes } from '../../types';
 import axios from 'axios';
 import { apiURI } from '../../clients/raiinmaker-api';
-import { useHistory } from 'react-router-dom';
+import { CircularProgress } from '@material-ui/core';
 
-const UserInfo: FC<UserTypes> = ({ userInfo }: UserTypes) => {
-  const history = useHistory();
-  const { user } = userInfo;
+const UserInfo: FC<UserTypes> = ({ userId }: UserTypes) => {
+  const [user, setUser] = useState<UserListType>();
+  const [loading, setLoading] = useState(false);
+  const [refetch, setRefetch] = useState(false);
+  const [deleteLoading, setDeleteLoading] = useState(false);
+  const [banLoading, setBanLoading] = useState(false);
+  const [unbanLoading, setUnbanLoading] = useState(false);
+  const [resetLoading, setResetLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      const userResponse = await axios.get(`${apiURI}/v1/user/single-user/${userId}`, { withCredentials: true });
+      setUser(userResponse.data.data);
+      setLoading(false);
+    };
+    fetchData();
+  }, [refetch]);
 
   // Handle active user
   const handleActiveUser = async () => {
+    setUnbanLoading(true);
     await axios.put(
       `${apiURI}/v1/user/update-user-status`,
       {
-        id: user.id,
+        id: user?.id,
         activeStatus: true,
       },
       { withCredentials: true },
     );
-    history.push('/dashboard/admin/userList');
+    setUnbanLoading(false);
+    setRefetch(!refetch);
   };
 
   // Handle banned user
   const handleBannedUser = async () => {
+    setBanLoading(true);
     await axios.put(
       `${apiURI}/v1/user/update-user-status`,
       {
-        id: user.id,
+        id: user?.id,
         activeStatus: false,
       },
       { withCredentials: true },
     );
-    history.push('/dashboard/admin/userList');
+    setBanLoading(false);
+    setRefetch(!refetch);
+  };
+
+  //Handle reset user password
+  const handleResetPassword = async () => {
+    setResetLoading(true);
+    await axios.put(`${apiURI}/v1/user/reset-user-password/${user?.id}`, {}, { withCredentials: true });
+    setResetLoading(false);
   };
 
   // Delete user
   const handleDeleteUser = async () => {
-    await axios.post(`${apiURI}/v1/user/delete-user-by-id/${user.id}`, {}, { withCredentials: true });
-    history.push('/dashboard/admin/userList');
+    setDeleteLoading(true);
+    await axios.post(`${apiURI}/v1/user/delete-user-by-id/${user?.id}`, {}, { withCredentials: true });
+    setDeleteLoading(false);
+    setRefetch(!refetch);
   };
 
   return (
     <div>
-      <div className={styles.userSideWrapper}>
-        <h2 className={headingStyles.headingSm}>User Details</h2>
-        <div className={styles.boxStyle}>
-          <h4>User Name:</h4>
-          <p>{user?.profile?.username}</p>
+      {loading && (
+        <div className={styles.loading}>
+          <CircularProgress />
         </div>
-        <div className={styles.boxStyle}>
-          <h4>Email:</h4>
-          <p>{user.email} </p>
-        </div>
-        <div className={styles.boxStyle}>
-          <h4>Kyc Status:</h4>
-          <p>{user.kycStatus}</p>
-        </div>
-        <div className={styles.boxStyle}>
-          <h4>Last Login:</h4>
-          <p>{new Date(user.lastLogin).toDateString() || 'Not login ever'}</p>
-        </div>
-        <div className={styles.boxStyle}>
-          <h4>User Status:</h4>
-          <p>{user.active === true ? 'Active' : 'Banned'}</p>
-        </div>
-        <div className={styles.boxStyle}>
-          <h4>Active Since:</h4>
-          <p>{new Date(user.createdAt).toDateString()}</p>
-        </div>
-        <div className={styles.boxStyle}>
-          <h4>Post Frequency:</h4>
-          <p>{user?.social_post?.length || 0}</p>
-        </div>
-        <div className={styles.userActiveButtonsWrapper}>
-          <div>
-            {user.active === false ? (
-              <CustomButton className={buttonStyles.buttonPrimary} onClick={handleActiveUser}>
-                Activate User
-              </CustomButton>
-            ) : (
-              <CustomButton className={buttonStyles.secondaryButton} onClick={handleBannedUser}>
-                Ban User
-              </CustomButton>
-            )}
+      )}
+      {user && (
+        <div className={styles.userSideWrapper}>
+          <h2 className={headingStyles.headingSm}>User Details</h2>
+          <div className={styles.boxStyle}>
+            <h4>User Name:</h4>
+            <p>{user?.profile?.username}</p>
           </div>
-          <CustomButton className={buttonStyles.secondaryButton} onClick={handleDeleteUser}>
-            Delete User
-          </CustomButton>
+          <div className={styles.boxStyle}>
+            <h4>Email:</h4>
+            <p>{user.email} </p>
+          </div>
+          <div className={styles.boxStyle}>
+            <h4>Kyc Status:</h4>
+            <p>{user.kycStatus}</p>
+          </div>
+          <div className={styles.boxStyle}>
+            <h4>Last Login:</h4>
+            <p>{new Date(user.lastLogin).toDateString() || 'Not login ever'}</p>
+          </div>
+          <div className={styles.boxStyle}>
+            <h4>User Status:</h4>
+            <p>{user.active === true ? 'Active' : 'Banned'}</p>
+          </div>
+          <div className={styles.boxStyle}>
+            <h4>Active Since:</h4>
+            <p>{new Date(user.createdAt).toDateString()}</p>
+          </div>
+          <div className={styles.boxStyle}>
+            <h4>Post Frequency:</h4>
+            <p>{user?.social_post?.length || 0}</p>
+          </div>
+          <div className={styles.userActiveButtonsWrapper}>
+            <div>
+              {user.active === false ? (
+                <CustomButton className={buttonStyles.buttonPrimary} onClick={handleActiveUser} loading={unbanLoading}>
+                  Activate User
+                </CustomButton>
+              ) : (
+                <CustomButton className={buttonStyles.secondaryButton} onClick={handleBannedUser} loading={banLoading}>
+                  Ban User
+                </CustomButton>
+              )}
+            </div>
+            <CustomButton className={buttonStyles.successButton} onClick={handleResetPassword} loading={resetLoading}>
+              Reset Password
+            </CustomButton>
+            <CustomButton className={buttonStyles.secondaryButton} onClick={handleDeleteUser} loading={deleteLoading}>
+              Delete User
+            </CustomButton>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
