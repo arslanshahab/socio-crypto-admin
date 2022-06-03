b34956186397794c191437fcc46db741bf9df7cc
Arslan Shahab <arslanshahab@gmail.com>
2022-06-03 18:02:22 +0500

    integrated orgDetails api at BrandList component

--- src/components/BrandList/BrandList.tsx
+++ src/components/BrandList/BrandList.tsx
@@ -1,12 +1,22 @@
-import React from 'react';
-import { useQuery } from '@apollo/client';
-import { ORG_DETAILS } from '../../operations/queries/admin';
-import { OrgDetails } from '../../types';
+import React, { useEffect, useState } from 'react';
 import styles from './brandList.module.css';
 import { CircularProgress } from '@material-ui/core';
+import axios from 'axios';
+import { apiURI } from '../../clients/raiinmaker-api';
 
 export const BrandList: React.FC = () => {
-  const { data, loading } = useQuery<OrgDetails>(ORG_DETAILS);
+  const [orgDetails, setOrgDetails] = useState([]);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    const fetchDate = async () => {
+      setLoading(true);
+      const { data } = await axios.get(`${apiURI}/v1/organization/org-details`, { withCredentials: true });
+      setOrgDetails(data.data.orgDetails);
+      setLoading(false);
+    };
+    fetchDate();
+  }, []);
+
   if (loading) {
     return (
       <div className={styles.loading}>
@@ -14,6 +24,7 @@ export const BrandList: React.FC = () => {
       </div>
     );
   }
+
   return (
     <div className={styles.brandListWrapper}>
       <h1 className={styles.heading}>Organization Details</h1>
@@ -28,15 +39,12 @@ export const BrandList: React.FC = () => {
             </tr>
           </thead>
           <tbody>
-            {data?.getOrgDetails?.map(
-              (
-                brand: { name: string; createdAt: string; campaignCount: number; adminCount: number },
-                index: number,
-              ) => (
+            {orgDetails?.map(
+              (brand: { name: string; createdAt: string; campaigns: number; admins: number }, index: number) => (
                 <tr className={styles.tableBodyRow} key={index}>
                   <td className={styles.tableColumn}>{brand.name}</td>
-                  <td className={styles.tableColumn}>{brand.campaignCount}</td>
-                  <td className={styles.tableColumn}>{brand.adminCount}</td>
+                  <td className={styles.tableColumn}>{brand.campaigns}</td>
+                  <td className={styles.tableColumn}>{brand.admins}</td>
                   <td className={styles.tableColumn}>{new Date(parseInt(brand.createdAt)).toLocaleString()}</td>
                 </tr>
               ),
