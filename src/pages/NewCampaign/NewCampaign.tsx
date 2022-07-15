f5b52254047d477a606ef6b1239706299c74b38e
Arslan Shahab <arslanshahab@gmail.com>
2022-07-15 21:12:57 +0500

    integrated update-campaign api

--- src/pages/NewCampaign/NewCampaign.tsx
+++ src/pages/NewCampaign/NewCampaign.tsx
@@ -1,10 +1,8 @@
 import { Box, LinearProgress } from '@material-ui/core';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { useHistory } from 'react-router';
 import { useDispatch } from 'react-redux';
-import { useMutation, FetchResult } from '@apollo/client';
-import { CampaignCreationResponse, NewCampaignVars, CampaignState, CampaignMediaSignedUrl } from '../../types.d';
-import { NEW_CAMPAIGN } from '../../operations/mutations/campaign';
+import { CampaignCreationResponse, CampaignState, CampaignMediaSignedUrl } from '../../types.d';
 import StepsView from '../../components/NewCampaign/StepsView';
 import StepContent from '../../components/NewCampaign/StepsContent';
 import useStoreCampaignSelector from '../../hooks/useStoreCampaignSelector';
@@ -40,8 +38,6 @@ const NewCampaignPage: React.FC = () => {
   const finalStep = steps.length - 1;
   const [activeStep, setActiveStep] = useState(firstStep);
   const campaign = useStoreCampaignSelector();
-
-  // const [saveCampaign] = useMutation<CampaignCreationResponse, NewCampaignVars>(NEW_CAMPAIGN);
 
   const handleNext = () => {
     setActiveStep((prevState) => (prevState < finalStep ? prevState + 1 : prevState));
@@ -94,44 +90,6 @@ const NewCampaignPage: React.FC = () => {
         { withCredentials: true },
       );
 
-      // const response: FetchResult<Record<string, any>, Record<string, any>> = await saveCampaign({
-      // variables: {
-      //   name: data.name,
-      //   coiinTotal: parseFloat(data.config.budgetType === 'raffle' ? '0' : data.config.coiinBudget),
-      //   target: data.target,
-      //   targetVideo: data.targetVideo || '',
-      //   beginDate: data.beginDate,
-      //   endDate: data.endDate,
-      //   symbol: data.config.cryptoSymbol.split('-')[0],
-      //   network: data.config.cryptoSymbol.split('-')[1],
-      //   description: data.description,
-      //   instructions: data.instructions,
-      //   company: userData.company,
-      //   isGlobal: campaign.config.isGlobal,
-      //   showUrl: campaign.config.showUrl,
-      //   algorithm: JSON.stringify(data.algorithm),
-      //   requirements:
-      //     data.config.budgetType === 'raffle' ? { ...data.requirements, email: true } : { ...data.requirements },
-      //   imagePath: data.campaignImage.filename,
-      //   campaignType: campaign.config.campaignType,
-      //   socialMediaType: campaign.config.socialMediaType,
-      //   tagline: data.tagline,
-      //   suggestedPosts: data.suggestedPosts,
-      //   suggestedTags: data.suggestedTags,
-      //   keywords: data.keywords,
-      //   type: data.config.budgetType || 'coiin',
-      //   campaignMedia: prepareMediaRequest(data.config.channelMedia),
-      //   campaignTemplates: prepareTemplateRequest(data.config.channelTemplates),
-      //   rafflePrize:
-      //     data.config.budgetType === 'raffle'
-      //       ? {
-      //           displayName: data.config.rafflePrizeName,
-      //           affiliateLink: data.config.rafflePrizeAffiliateLink,
-      //           image: data.config.raffleImage.filename,
-      //         }
-      //       : undefined,
-      // },
-      // });
       if (response.data.data) {
         const newCampaign: CampaignCreationResponse = response.data.data;
         if (newCampaign.campaignImageSignedURL) {
