fd38a88e1c9268cbc0da944ad594a849749c3202
Arslan Shahab <arslanshahab@gmail.com>
2022-07-20 20:52:39 +0500

    integrated new-user api

--- src/components/RegisterUser.tsx
+++ src/components/RegisterUser.tsx
@@ -1,18 +1,12 @@
-import React, { useState } from 'react';
-import { useMutation } from '@apollo/client';
-import { NEW_USER } from '../operations/queries/admin';
-import {
-  CircularProgress,
-  Dialog,
-  DialogContent,
-  FormControlLabel,
-  Radio,
-  RadioGroup,
-  TextField,
-} from '@material-ui/core';
+import React, { ChangeEvent, useState } from 'react';
+import { Dialog, DialogContent, FormControlLabel, Radio, RadioGroup, TextField } from '@material-ui/core';
 import CustomButton from './CustomButton';
 import buttonStyles from '../assets/styles/customButton.module.css';
 import headingStyles from '../assets/styles/heading.module.css';
+import axios from 'axios';
+import { apiURI } from '../clients/raiinmaker-api';
+import { useDispatch } from 'react-redux';
+import { showErrorAlert, showSuccessAlert } from '../store/actions/alerts';
 
 interface Props {
   open: boolean;
@@ -20,42 +14,46 @@ interface Props {
 }
 
 interface FormItems {
+  [key: string]: string;
   email: string;
   name: string;
   role: string;
 }
 
 export const RegisterUser: React.FC<Props> = ({ open, setOpen }) => {
-  const [values, setValues] = useState({
+  const dispatch = useDispatch();
+  const [values, setValues] = useState<FormItems>({
     email: '',
     name: '',
     role: '',
-  } as FormItems);
-  const [role, setRole] = useState('');
-
-  const [newUser, { loading, error }] = useMutation(NEW_USER, {
-    variables: { name: values.name, email: values.email, role },
   });
+  const [loading, setLoading] = useState(false);
 
-  const handleChange = (event: any) => {
-    event.persist();
-    setValues((values) => ({
-      ...values,
-      [event.target.name]: event.target.value,
-    }));
+  const handleChange = (event: ChangeEvent<any>) => {
+    const data = { ...values };
+    data[event.target.name] = event.target.value;
+    setValues(data);
   };
 
   const handleClose = () => {
     setOpen((open) => !open);
   };
 
-  const handleSubmit = async (event: any) => {
+  const handleSubmit = async () => {
+    debugger;
     try {
-      await newUser();
-      if (error) throw new Error(`user registration error: ${error}`);
+      setLoading(true);
+      axios.post(
+        `${apiURI}/v1/organization/new-user`,
+        { name: values.name, email: values.email, role: values.role },
+        { withCredentials: true },
+      );
+      setLoading(false);
+      dispatch(showSuccessAlert('User created successfully!'));
       setOpen((open) => !open);
     } catch (e) {
       console.log(e);
+      dispatch(showErrorAlert('Something went wrong. Please try again!'));
     }
   };
 
@@ -68,6 +66,7 @@ export const RegisterUser: React.FC<Props> = ({ open, setOpen }) => {
             control={<Radio color={'primary'} style={{ padding: '0px' }} />}
             label={'Admin'}
             value={'admin'}
+            name="role"
             onChange={handleChange}
             labelPlacement={'top'}
             className="text-blue-800 p-0"
@@ -77,6 +76,7 @@ export const RegisterUser: React.FC<Props> = ({ open, setOpen }) => {
             control={<Radio color={'primary'} style={{ padding: '0px' }} />}
             label={'Manager'}
             value={'manager'}
+            name="role"
             onChange={handleChange}
             className="text-blue-800"
           />
@@ -87,8 +87,8 @@ export const RegisterUser: React.FC<Props> = ({ open, setOpen }) => {
         </div>
       </DialogContent>
       <div className={`${buttonStyles.buttonWrapper} mb-4`}>
-        <CustomButton className={buttonStyles.buttonPrimary} onClick={handleSubmit}>
-          {loading ? <CircularProgress color="inherit" size={20} /> : 'Create'}
+        <CustomButton className={buttonStyles.buttonPrimary} onClick={handleSubmit} loading={loading}>
+          Create
         </CustomButton>
         <CustomButton className={buttonStyles.buttonPrimary} onClick={handleClose}>
           Cancel
