0b11f4c4e47f458e8ee17945140f4a7175b3ae86
Arslan Shahab <arslanshahab@gmail.com>
2022-08-09 00:26:01 +0500

    return error message if invalid credit card

--- src/components/CardSection.tsx
+++ src/components/CardSection.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { CardElement } from '@stripe/react-stripe-js';
 
 const CARD_ELEMENT_OPTIONS = {
@@ -20,9 +20,17 @@ const CARD_ELEMENT_OPTIONS = {
 };
 
 export const CardSection: React.FC = () => {
+  const [error, setError] = React.useState<string | null>(null);
+  const handleOnChange = (e: any) => {
+    if (e.error) return setError(e.error.message);
+    setError(null);
+  };
   return (
-    <label>
-      <CardElement options={CARD_ELEMENT_OPTIONS} />
-    </label>
+    <Fragment>
+      <label>
+        <CardElement options={CARD_ELEMENT_OPTIONS} onChange={handleOnChange} />
+      </label>
+      {error && <p className="text-xs text-red-600 mt-3 mb-0">The credit card you have entered is invalid</p>}
+    </Fragment>
   );
 };
