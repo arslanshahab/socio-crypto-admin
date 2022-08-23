677de58da656a8a997420a8700630703d4160d3d
Arslan Shahab <arslanshahab@gmail.com>
2022-08-23 21:24:01 +0500

    apply sorting for participant

--- src/components/Campaigns/CampaignParticipants.tsx
+++ src/components/Campaigns/CampaignParticipants.tsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import React, { ChangeEvent, useEffect, useState } from 'react';
 import { apiURI } from '../../clients/raiinmaker-api';
 import tableStyles from '../../assets/styles/table.module.css';
 import CustomButton from '../CustomButton';
@@ -41,12 +41,13 @@ const CampaignParticipants: React.FC = () => {
   const [total, setTotal] = useState(0);
   const [searchLoading, setSearchLoading] = useState(false);
   const [reload, setReload] = useState(false);
+  const [sort, setSort] = useState('desc');
 
   useEffect(() => {
     const fetchCampaignParticipants = async () => {
       filter ? setSearchLoading(true) : setIsLoading(true);
       const { data } = await axios.get(
-        `${apiURI}/v1/participant/all?skip=${skip}&take=${take}&campaignId=${id}&filter=${filter}`,
+        `${apiURI}/v1/participant/all?skip=${skip}&take=${take}&campaignId=${id}&filter=${filter}&sort=${sort}`,
         {
           withCredentials: true,
         },
@@ -57,7 +58,7 @@ const CampaignParticipants: React.FC = () => {
       setSearchLoading(false);
     };
     fetchCampaignParticipants();
-  }, [id, filter, skip, reload]);
+  }, [id, filter, skip, reload, sort]);
 
   // Blacklist a participant
   const blacklistParticpant = async (participantId: string) => {
@@ -76,11 +77,6 @@ const CampaignParticipants: React.FC = () => {
     }
   };
 
-  // Search record
-  const handleSearchRecord = () => {
-    setFilter(searchData);
-  };
-
   // Handle Key Press
   const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === 'Enter') {
@@ -93,19 +89,30 @@ const CampaignParticipants: React.FC = () => {
     setSkip(skip);
   };
 
-  if (isLoading) {
-    return (
-      <div className={tableStyles.loading}>
-        <CircularProgress />
-      </div>
-    );
-  }
+  // Handle sort
+  const handleSort = (e: ChangeEvent<HTMLSelectElement>) => {
+    if (e.target.value === 'desc') setSort('desc');
+    if (e.target.value === 'asc') setSort('asc');
+  };
 
   return (
     <div>
       <div className="flex justify-between items-center pb-4">
         <h2 className={headingStyles.heading}>Participant List</h2>
         <div className="flex gap-4">
+          <div className="flex gap-4 items-center text-gray-500 text-sm">
+            <p>Sort by Participation Score</p>
+            <div className="inset-y-0 right-0 flex items-center">
+              <select
+                name="sorting"
+                className="focus:ring-indigo-500 focus:border-indigo-500 h-full py-0 pl-2 pr-7 border-transparent bg-transparent text-gray-500 sm:text-sm rounded-md"
+                onChange={handleSort}
+              >
+                <option value={'desc'}>DESC</option>
+                <option value={'asc'}>ASC</option>
+              </select>
+            </div>
+          </div>
           <input
             type="text"
             name="search"
@@ -115,62 +122,62 @@ const CampaignParticipants: React.FC = () => {
             onChange={handleSearchField}
             onKeyPress={handleKeyPress}
           />
-          <CustomButton className={buttonStyles.buttonPrimary} onClick={handleSearchRecord} loading={searchLoading}>
+          <CustomButton className={buttonStyles.buttonPrimary} loading={searchLoading}>
             Search
           </CustomButton>
         </div>
       </div>
+      {isLoading ? (
+        <div className={tableStyles.tableBodyLoading}>
+          <CircularProgress />
+        </div>
+      ) : (
+        <div className={tableStyles.tableWrapper}>
+          <table className={tableStyles.table}>
+            <thead>
+              <tr className={tableStyles.tableHeadRow}>
+                <th className={tableStyles.tableColumn}>Username</th>
+                <th className={tableStyles.tableColumn}>Email</th>
+                <th className={tableStyles.tableColumn}>Post Count</th>
+                <th className={tableStyles.tableColumn}>Like Score</th>
+                <th className={tableStyles.tableColumn}>Share Score</th>
+                <th className={`${tableStyles.tableColumn} cursor-pointer`}>Participation Score</th>
+                <th className={tableStyles.tableColumn}>Total Likes</th>
+                <th className={tableStyles.tableColumn}>Total Shares</th>
+                <th className={tableStyles.tableColumn}>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {!participants.length && <p className="p-2">No participant found for this campaign.</p>}
+              {participants &&
+                participants.map((participant: Participant) => (
+                  <tr className={tableStyles.tableBodyRow} key={participant.id}>
+                    <td className={tableStyles.tableColumn}>{participant.username}</td>
+                    <td className={tableStyles.tableColumn}>{participant.email}</td>
+                    <td className={tableStyles.tableColumn}>{participant.selfPostCount}</td>
+                    <td className={tableStyles.tableColumn}>{participant.likeScore}</td>
+                    <td className={tableStyles.tableColumn}>{participant.shareScore}</td>
+                    <td className={tableStyles.tableColumn}>{participant.participationScore}</td>
+                    <td className={tableStyles.tableColumn}>{participant.totalLikes}</td>
+                    <td className={tableStyles.tableColumn}>{participant.totalShares}</td>
+                    <td className={tableStyles.tableColumn}>
+                      {!participant.blacklist && (
+                        <CustomButton
+                          className={buttonStyle.secondaryButton}
+                          onClick={() => blacklistParticpant(participant.id)}
+                          loading={blacklistLoading && participantId === participant.id}
+                        >
+                          Blacklist
+                        </CustomButton>
+                      )}
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        </div>
+      )}
 
-      <div className={tableStyles.tableWrapper}>
-        <table className={tableStyles.table}>
-          <thead>
-            <tr className={tableStyles.tableHeadRow}>
-              <th className={tableStyles.tableColumn}>Username</th>
-              <th className={tableStyles.tableColumn}>Email</th>
-              <th className={tableStyles.tableColumn}>Twitter Username</th>
-              <th className={tableStyles.tableColumn}>Post Count</th>
-              <th className={tableStyles.tableColumn}>Like Score</th>
-              <th className={tableStyles.tableColumn}>Share Score</th>
-              <th className={tableStyles.tableColumn}>Participation Score</th>
-              <th className={tableStyles.tableColumn}>Total Likes</th>
-              <th className={tableStyles.tableColumn}>Total Shares</th>
-              <th className={tableStyles.tableColumn}>Created At</th>
-              <th className={tableStyles.tableColumn}>Last Login</th>
-              <th className={tableStyles.tableColumn}>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            {!participants.length && <p className="p-2">No participant found for this campaign.</p>}
-            {participants &&
-              participants.map((participant: Participant) => (
-                <tr className={tableStyles.tableBodyRow} key={participant.id}>
-                  <td className={tableStyles.tableColumn}>{participant.username}</td>
-                  <td className={tableStyles.tableColumn}>{participant.email}</td>
-                  <td className={tableStyles.tableColumn}>{participant.twitterUsername}</td>
-                  <td className={tableStyles.tableColumn}>{participant.selfPostCount}</td>
-                  <td className={tableStyles.tableColumn}>{participant.likeScore}</td>
-                  <td className={tableStyles.tableColumn}>{participant.shareScore}</td>
-                  <td className={tableStyles.tableColumn}>{participant.participationScore}</td>
-                  <td className={tableStyles.tableColumn}>{participant.totalLikes}</td>
-                  <td className={tableStyles.tableColumn}>{participant.totalShares}</td>
-                  <td className={tableStyles.tableColumn}>{new Date(participant.createdAt).toDateString()}</td>
-                  <td className={tableStyles.tableColumn}>{new Date(participant.lastLogin).toDateString()}</td>
-                  <td className={tableStyles.tableColumn}>
-                    {!participant.blacklist && (
-                      <CustomButton
-                        className={buttonStyle.secondaryButton}
-                        onClick={() => blacklistParticpant(participant.id)}
-                        loading={blacklistLoading && participantId === participant.id}
-                      >
-                        Blacklist
-                      </CustomButton>
-                    )}
-                  </td>
-                </tr>
-              ))}
-          </tbody>
-        </table>
-      </div>
       {total > take && <Pagination total={total} skip={skip} take={take} getValue={getValue} />}
     </div>
   );
