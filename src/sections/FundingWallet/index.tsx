2aa8bd0070b7a1d26b2e83593a69430268123d95
Arslan Shahab <arslanshahab@gmail.com>
2022-09-10 01:03:41 +0500

    show coiin currency

--- src/sections/FundingWallet/index.tsx
+++ src/sections/FundingWallet/index.tsx
@@ -7,19 +7,22 @@ import Divider from '../../componentsv2/Divider';
 import { ApiClient } from '../../services/apiClient';
 import { useDispatch } from 'react-redux';
 import { showErrorAlert } from '../../store/actions/alerts';
-import { FundingWallet } from '../../types';
+import { FundingWallet as FundingWalletTypes } from '../../types';
 
 const FundingWallet: FC = () => {
   const dispatch = useDispatch();
   const [purchaseCoiin, setPurchaseCoiin] = useState(false);
-  const [fundingWallet, setFundingWallet] = useState<FundingWallet[]>([]);
+  const [fundingWallet, setFundingWallet] = useState<FundingWalletTypes[]>([]);
   const [isWalletLoading, setIsWalletLoading] = useState(false);
+  const [coiins, setCoiins] = useState<FundingWalletTypes[]>([]);
 
   useEffect(() => {
     const fetchData = async () => {
       setIsWalletLoading(true);
       await ApiClient.getFundingWallet()
         .then((resp) => {
+          const filterCoiin = resp.filter((item) => item.type === 'COIIN');
+          setCoiins(filterCoiin);
           setFundingWallet(resp);
         })
         .catch((error) => {
@@ -32,23 +35,35 @@ const FundingWallet: FC = () => {
     fetchData();
   }, []);
 
+  const toolTipMap = {
+    title: 'Token',
+    value: 'Balance',
+    network: 'Network',
+  };
+
   return (
-    <div className="border-2 border-denimBlue rounded-3xl">
+    <div className="border-2 border-denimBlue rounded-3xl p-4">
       <h1 className="text-xl text-denimBlue">Wallet</h1>
       <Divider />
       <PurchaseDialog open={purchaseCoiin} setOpen={setPurchaseCoiin} />
-      <CustomButton onClick={() => setPurchaseCoiin(true)} className="bg-cyberYellow rounded-full px-3 py-2 w-auto">
+      <CustomButton
+        onClick={() => setPurchaseCoiin(true)}
+        className="bg-cyberYellow rounded-full px-3 py-2 w-auto my-6"
+      >
         Purchase Coiin Points
       </CustomButton>
-      {/* <PrimaryCard
-        title={currency.type}
-        network={currency.network}
-        value={currency.balance}
-        icon={currency.symbolImageUrl}
-        tooltipTitle={toolTipMap.title}
-        tooltipValue={toolTipMap.value}
-        tooltipNetwork={toolTipMap.network}
-      /> */}
+      {coiins?.map((coiin: FundingWalletTypes) => (
+        <PrimaryCard
+          key={coiin.type}
+          title={coiin.type}
+          network={coiin.network}
+          value={coiin.balance}
+          icon={coiin.symbolImageUrl}
+          tooltipTitle={toolTipMap.title}
+          tooltipValue={toolTipMap.value}
+          tooltipNetwork={toolTipMap.network}
+        />
+      ))}
     </div>
   );
 };
