8c81c87d487e96bea91aade9e27f848c7ca06aac
Arslan Shahab <arslanshahab@gmail.com>
2022-10-07 16:15:24 +0500

    create linechart component

---
+++ src/componentsv2/LineChart/index.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import './lineChart.scss';
+
+const LineChart: FC = () => {
+  return (
+    <div>
+      <p>line chart</p>
+    </div>
+  );
+};
+
+export default LineChart;
