6c5860789146390510afcbda793fd38b3d68da16
Arslan Shahab <arslanshahab@gmail.com>
2022-10-31 13:52:35 +0500

    hide CampaignRequirement page

--- src/components/NewCampaign/StepsContent.tsx
+++ src/components/NewCampaign/StepsContent.tsx
@@ -3,7 +3,7 @@ import { CampaignState } from '../../types.d';
 import CampaignSetupForm from '../Forms/CampaignSetupForm';
 import CampaignInitializeForm from '../Forms/CampaignInitializeForm';
 import CampaignPostAnndTagsForm from '../Forms/CampaignPostsForm';
-import CampaignRequirementsForm from '../Forms/CampaignRequirementsForm';
+// import CampaignRequirementsForm from '../Forms/CampaignRequirementsForm';
 import CampaignAlgorithmForm from '../Forms/CampaignAlgorithmForm';
 import PreviewScreen from './PreviewScreen';
 import CampaignMediaForm from '../Forms/CampaignMediaForm/CampaignMediaForm';
@@ -76,16 +76,16 @@ const StepContent: React.FC<Props & ActionsProps> = ({
           handleNext={handleNext}
         />
       );
-    case 4:
-      return (
-        <CampaignRequirementsForm
-          activeStep={activeStep}
-          firstStep={firstStep}
-          finalStep={finalStep}
-          handleBack={handleBack}
-          handleNext={handleNext}
-        />
-      );
+    // case 4:
+    //   return (
+    //     <CampaignRequirementsForm
+    //       activeStep={activeStep}
+    //       firstStep={firstStep}
+    //       finalStep={finalStep}
+    //       handleBack={handleBack}
+    //       handleNext={handleNext}
+    //     />
+    //   );
     case 5:
       return (
         <CampaignAlgorithmForm
